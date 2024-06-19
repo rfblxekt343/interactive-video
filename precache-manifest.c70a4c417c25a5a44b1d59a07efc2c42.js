b86b366f4b6125e48067a13ede50660188bb9c69
rfblxekt343 <almalevy100@gmail.com>
2024-06-19 17:15:32 +0300

    Updates

---
+++ precache-manifest.c70a4c417c25a5a44b1d59a07efc2c42.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e2356d81c017725245490e54942a344",
+    "url": "/interactive-video/index.html"
+  },
+  {
+    "revision": "a2556af89bfc73041cef",
+    "url": "/interactive-video/static/css/main.95359380.chunk.css"
+  },
+  {
+    "revision": "1437e4dc67f916aab1f8",
+    "url": "/interactive-video/static/js/2.e9935e1c.chunk.js"
+  },
+  {
+    "revision": "a2556af89bfc73041cef",
+    "url": "/interactive-video/static/js/main.f2558712.chunk.js"
+  },
+  {
+    "revision": "fd63769dbb87c0f314d5",
+    "url": "/interactive-video/static/js/runtime~main.0c2903d7.js"
+  },
+  {
+    "revision": "6ed5eaf5839eb45afd2e67e1fbe86633",
+    "url": "/interactive-video/static/media/video1.6ed5eaf5.mp4"
+  },
+  {
+    "revision": "7a4200e4fc2b8bf8c66a881042c2cee3",
+    "url": "/interactive-video/static/media/video2.7a4200e4.mp4"
+  },
+  {
+    "revision": "21c9cfc27ee2b456f4ee8755195736e3",
+    "url": "/interactive-video/static/media/video3.21c9cfc2.mp4"
+  },
+  {
+    "revision": "e9fe85c24703f5a9e713d77c4101c2d9",
+    "url": "/interactive-video/static/media/video4.e9fe85c2.mp4"
+  },
+  {
+    "revision": "c26aff024797de09f8ecadeddfa1bcf8",
+    "url": "/interactive-video/static/media/video5.c26aff02.mp4"
+  },
+  {
+    "revision": "167abe3fb488936b8df9b7544df79683",
+    "url": "/interactive-video/static/media/video6.167abe3f.mp4"
+  },
+  {
+    "revision": "8616c756d6cf63ca4c478b0d70fcfc9a",
+    "url": "/interactive-video/static/media/video7.8616c756.mp4"
+  },
+  {
+    "revision": "4a59f782c91672782ba594ab3e710930",
+    "url": "/interactive-video/static/media/video8.4a59f782.mp4"
+  }
+]);
